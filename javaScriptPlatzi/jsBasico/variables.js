a417b604987a203689444ea064b194e7ceed89d5
Yamal Márquez <yamal.marquez.e.229@gmail.com>
2021-03-04 11:16:42 -0600

    Cambio de carpetas

---
+++ javaScriptPlatzi/jsBasico/variables.js
@@ -0,0 +1,21 @@
+var nombre = "Oscar";
+
+// declarar
+var edad;
+
+// llamar a la variable
+edad    // el valor esperado es "undefined" ya que no hemos asignado algún valor.
+
+// inicializar
+edad = 30;
+
+edad    // el valor esperado es "30".
+
+// variable tipo objeto; Array.
+var elementos = ['Computadora', 'Celular'];
+
+// variable tipo objeto; Objeto.
+var persona = {
+    nombre: 'Yamal',
+    cel: 2381098888
+};
